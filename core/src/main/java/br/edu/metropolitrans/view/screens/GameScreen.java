f2a2f247b88d731472004cf1c162c343abb36057
marianacvn <marianacvn1@gmail.com>
2025-02-02 14:12:21 -0300

    Remoção de import

--- core/src/main/java/br/edu/metropolitrans/view/screens/GameScreen.java
+++ core/src/main/java/br/edu/metropolitrans/view/screens/GameScreen.java
@@ -1,7 +1,6 @@
 package br.edu.metropolitrans.view.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
